3d7eaf7ca843cf81a93c53faf21237cf2d19f8a7
JBatsyus <jkalabina@gmail.com>
2021-08-16 12:03:40 +0200

    Доработка тестового задания №1

--- pr1.js
+++ pr1.js
@@ -1,15 +1,15 @@
-
-
-    
 function check() {
     let elem = document.getElementById('input');
     let elems = document.getElementsByName('name');
-    if (elem.value !== 0) {
+    if (elem.value) {
         let str = elem.value;
+        str = str.trim().replace(/ +/g, ' ');
         let arr = str.split(" ");
         for (let i = 0; i < elems.length; i++) {
-            elems[i].value = arr[i];
-         }
+            let string = arr[i];
+            let lower = string.toLowerCase();
+            let firstLetter = lower[0].toUpperCase() + lower.slice(1);
+            elems[i].value = firstLetter;
+        }
     }
 }
-
