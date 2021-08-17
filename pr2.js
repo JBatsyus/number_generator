574273241a3be0ad33e1f6853261d63382406660
JBatsyus <jkalabina@gmail.com>
2021-08-17 15:44:28 +0200

    Доработка практики №2

--- pr2.js
+++ pr2.js
@@ -1,16 +1,13 @@
 
 
 function checkSpam() {
-    let userCommet = document.getElementById("add_com");
+ let userCommet = document.getElementById("add_com");
     let otvet = document.querySelector(".tasks-container");
+    
     if (userCommet.value) {
         let str = userCommet.value;
         str = str.toLowerCase();
-      
- }
-
-    // if (!userCommet.value) return;
-    otvet.innerHTML = userCommet.value;
-    
+        let firstLet = str.replaceAll(/(viagra|xxx)/g,'***');
+        otvet.innerHTML = firstLet;
+    }
 }
-
