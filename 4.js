620209e61a255c4d16338cbd76cc65ed29ed49c0
JBatsyus <jkalabina@gmail.com>
2021-08-20 15:31:45 +0200

    Коррекция ДЗ

--- 4.js
+++ 4.js
@@ -1,41 +1,42 @@
+function generate() {
+// очищаем
+    document.getElementById('randomNumb').innerHTML = "";
+    document.getElementById('minNum').innerHTML = "";
+    document.getElementById('maxNum').innerHTML = "";
+    document.getElementById('averageNum').innerHTML = "";
+    document.getElementById('sumNum').innerHTML = "";
+    document.getElementById('multik').innerHTML = "";
 
+    // генерация случайных чисел в заданном промежутке
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    // массив от -10 до 10
+    let randomNumb = [];
+    for (let i = 0; i < 10; i++) {
+        randomNumb.push(getRandomIntInclusive(-10, 10));
+        document.getElementById('randomNumb').innerHTML = randomNumb;
+    }
+
+    // минимальное число
+    let arr = randomNumb;
+    let minNum = Math.min.apply(null, arr);
+    document.getElementById('minNum').innerHTML += minNum;
+    // максимальное число
+    let maxNum = Math.max.apply(null, arr);
+    document.getElementById('maxNum').innerHTML += maxNum;
+    // среднее арифметическое
+    let averageNum = arr.reduce((a, b) => a + b, 0) / arr.length;
+    document.getElementById('averageNum').innerHTML += averageNum;
+    // сумма чисел
+    let sumNum = arr.reduce((a, b) => a + b, 0);
+    document.getElementById('sumNum').innerHTML += sumNum;
+    // произведение чисел
+    let multik = arr.reduce((a, b) => a * b);
+    document.getElementById('multik').innerHTML += multik;
 }
-
-
-let randomNumb = [];
-for (let i = 0; i < 10; i++) {
-    randomNumb.push(getRandomIntInclusive(-10, 10));
-    document.getElementById('randomNumb').innerHTML = randomNumb;
-}
-
-
-let arr = randomNumb;
-let minNum = Math.min.apply(null, arr);
-document.getElementById('minNum').innerHTML = minNum;
-
-let maxNum = Math.max.apply(null, arr);
-document.getElementById('maxNum').innerHTML = maxNum;
-
-let averageNum = arr.reduce((a, b) => a + b, 0) / arr.length;
-document.getElementById('averageNum').innerHTML = averageNum;
-
-let sumNum =  arr.reduce((a, b) => a + b, 0);
-document.getElementById('sumNum').innerHTML = sumNum;
- 
-let multik =  arr.reduce((a, b) => a * b);
-document.getElementById('multik').innerHTML = multik;
-
-// document.getElementById('randomNumb').innerHTML = "";
-// document.getElementById('minNum').innerHTML = "";
-// document.getElementById('maxNum').innerHTML = "";
-// document.getElementById('averageNum').innerHTML = "";
-// document.getElementById('sumNum').innerHTML = "";
-// document.getElementById('multik').innerHTML = "";
-
 
 
--- pr2.js
+++ pr2.js
@@ -1,13 +1,10 @@
-
-
 function checkSpam() {
- let userCommet = document.getElementById("add_com");
+    let userCommet = document.getElementById("add_com");
     let otvet = document.querySelector(".tasks-container");
-    
     if (userCommet.value) {
         let str = userCommet.value;
         str = str.toLowerCase();
-        let firstLet = str.replaceAll(/(viagra|xxx)/g,'***');
+        let firstLet = str.replaceAll(/(viagra|xxx)/g, '***');
         otvet.innerHTML = firstLet;
     }
 }
